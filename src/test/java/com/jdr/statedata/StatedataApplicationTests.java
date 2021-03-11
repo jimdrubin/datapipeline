6c937896e8915a38cb97a3d420a75214162bd942
jimdrubin <8015210+jimdrubin@users.noreply.github.com>
2021-03-11 11:36:11 -0800

    Initial Push

--- src/test/java/com/jdr/statedata/StatedataApplicationTests.java
+++ src/test/java/com/jdr/statedata/StatedataApplicationTests.java
@@ -3,11 +3,17 @@ package com.jdr.statedata;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class StatedataApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void correct_number_of_districts() {
+		ReadExcel reade = new ReadExcel();
+		District[] dists = reade.getDistrictData();
+		assertEquals(436,dists.length);
 	}
 
 }
